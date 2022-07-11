63078506629293e4ac9a53119597f67c2bb5345f
Pariya <87968419+pariyajebreili@users.noreply.github.com>
2022-07-11 18:38:05 +0430

    Update ALU32Bit.v

--- ALU32Bit.v
+++ ALU32Bit.v
@@ -1,20 +1,18 @@
-`define ADD = 4'b0000;
-`define SUB = 4'b0001;
-`define AND = 4'b0010;
-`define OR = 4'b0011;
+`define ADD  4'b0000;
+`define SUB  4'b0001;
+`define AND  4'b0010;
+`define OR   4'b0011;
 
 
-module ALU32Bit(Operand1, Operand2, ALUControl, shiftAmount, overFlow, zero, ALUResult, reset);
+module ALU32Bit(Operand1, Operand2, ALUControl, shiftAmount, zero, ALUResult, reset);
 
 	input wire reset;
 	input wire signed [31:0] Operand1,Operand2;
 	input wire [3:0] ALUControl;
 	input wire [4:0] shiftAmount;
-	output reg overFlow, zero;
+	output reg  zero;
 	output reg signed [31:0] ALUResult;
 
-	wire [31:0] neg_Operand2;
-	assign neg_Operand2 = -Operand2;
 
 	always @(posedge reset) zero <= 1'b0;
 
@@ -22,27 +20,19 @@ module ALU32Bit(Operand1, Operand2, ALUControl, shiftAmount, overFlow, zero, ALU
 	begin
 
 		if(Operand1 == Operand2)
-		zero <= 1'b1;
+			zero <= 1'b1;
 		else
-		zero <= 1'b0;
+			zero <= 1'b0;
 
 		case(ALUControl)
 
 			0000: begin	
 				ALUResult <= Operand1 + Operand2;
-				if(Operand1[31] == Operand2[31] && ALUResult[31] == ~Operand1[31])
-				overFlow <= 1'b1;
-				else
-				overFlow <= 1'b0;
 				end
 
 			0001:
 				begin
-				ALUResult <= Operand1 + neg_Operand2;
-				if(Operand1[31] == neg_Operand2[31] && ALUResult[31] == ~Operand1[31])
-				overFlow <= 1'b1;
-				else
-				overFlow <= 1'b0;
+				ALUResult <= Operand1 - Operand2;
 				end
 				
 			0010:
